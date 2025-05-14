53248488fa1d2994e1cedac56935b7d20931d787
Shashikiran200212 <134192630+Shashikiran200212@users.noreply.github.com>
2025-05-14 09:18:58 +0530

    Update [id].tsx

--- app/tasks/[id].tsx
+++ app/tasks/[id].tsx
@@ -1,11 +1,11 @@
 import {View, Text} from 'react-native'
 import React from 'react'
 
-const MovieDetails  = () => {
+const TaskDetails  = () => {
     return (
         <View>
             <Text></Text>
         </View>
     )
 }
-    export default MovieDetails
+    export default TaskDetails
